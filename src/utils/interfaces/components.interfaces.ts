0b1ff0db9ab99b7beb09f6bc270c5e251851dc54
Santiago Suárez <santiago.suarez@axcelsoftware.com>
2021-10-06 08:53:30 -0500

    Se agregan interfaces general, pendientes de separación

--- src/utils/interfaces/components.interfaces.ts
+++ src/utils/interfaces/components.interfaces.ts
@@ -6,7 +6,9 @@ export interface ICardProps {
 export interface IItemProject {
 	id: string;
 	name: string;
+	dependency?: string;
 	creationDate: string;
+	createdBy?: string;
 }
 
 export interface IUpdatedValues {
@@ -27,6 +29,7 @@ export interface IProjectAttributes {
 	id: string;
 	name: string;
 	description: string;
+	dependency: string;
 	audit_trail: IAuditTrail;
 	status: number;
 }
@@ -38,4 +41,47 @@ export interface IProjectsResponse {
 export interface IProjectResponse {
 	data: IProjectAttributes;
 	message: string;
+}
+
+export interface IRealEstateAttributes {
+	id?: number;
+	dependency: string;
+	destination_type: string;
+	accounting_account: string;
+	cost_center: string;
+
+	registry_number: string;
+	name: string;
+	description: string;
+	address?: string;
+	cbml?: string;
+
+	total_area: number;
+	total_percentage: number;
+	estate_type: string;
+	tipology: string;
+
+	project_id: number;
+
+	status?: number;
+	audit_trail?: IAuditTrail;
+}
+
+export interface IRealEstatesResponse {
+	data: IRealEstateAttributes[];
+	message: string;
+}
+
+export interface IRealEstateResponse {
+	data: IRealEstateAttributes;
+	message: string;
+}
+
+export interface IItemRealEstate {
+	id: string;
+	matricula: string | number;
+	name: string;
+	dependency?: string;
+	creationDate: string;
+	createdBy?: string;
 }
