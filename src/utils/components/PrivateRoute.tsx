225a63bbe6ce48f557e99d1c17bfc4c6824b85b0
Santiago Suárez <santiago.suarez@axcelsoftware.com>
2021-10-06 13:35:57 -0500

    Se eliminan líneas de código innecesarias

--- src/utils/components/PrivateRoute.tsx
+++ src/utils/components/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import { Component, createElement } from 'react';
+import { createElement } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 // import { useRecoilValue } from 'recoil';
 
