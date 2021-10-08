edbfea013174bfe36876876e39b7c8073066fdda
Santiago Suárez <santiago.suarez@axcelsoftware.com>
2021-10-08 08:21:10 -0500

    Se añade vista de Bienes Inmuebles asociados

--- src/views/MasterTemplate/pages/Projects/components/DetailProject.tsx
+++ src/views/MasterTemplate/pages/Projects/components/DetailProject.tsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { getProject } from '../../../../../apis/uabi';
+import { Link, useParams } from 'react-router-dom';
+import { Table } from 'semantic-ui-react';
+import {
+	getProject,
+	getRealEstates,
+	getRealEstatesByProject,
+} from '../../../../../apis/uabi';
+import ItemRealEstate from '../../RealEstate/components/ItemRealEstate';
 import {
 	IProjectAttributes,
 	IProjectResponse,
 	IProjectsResponse,
+	IRealEstatesResponse,
 } from './../../../../../utils/interfaces/components.interfaces';
 
 interface IProps {
@@ -28,6 +35,47 @@ const DetailProject = () => {
 		status: -1,
 	});
 
+	const [realEstates, setRealEstates] = useState<any[]>([
+		{
+			id: -1,
+			dependency: '',
+			destination_type: '',
+			accounting_account: '',
+			cost_center: '',
+
+			registry_number: '',
+			name: '',
+			description: '',
+
+			total_area: -1,
+			total_percentage: -1,
+			estate_type: '',
+			tipology: '',
+
+			project_id: -1,
+
+			audit_trail: {
+				created_by: '',
+				created_on: '',
+				updated_by: null,
+				updated_on: null,
+				updated_values: null,
+			},
+			status: -1,
+		},
+	]);
+
+	const _getRealEstates = async () => {
+		let realEstatesResponse: IRealEstatesResponse | string =
+			await getRealEstatesByProject(id);
+
+		if (typeof realEstatesResponse !== 'string') {
+			let tmpData = realEstatesResponse.data;
+
+			setRealEstates(tmpData);
+		}
+	};
+
 	const _getProject = async () => {
 		let projectResponse: IProjectResponse | string = await getProject(id);
 
@@ -40,6 +88,7 @@ const DetailProject = () => {
 
 	useEffect(() => {
 		_getProject();
+		_getRealEstates();
 	}, []);
 
 	return (
@@ -60,7 +109,7 @@ const DetailProject = () => {
 							<hr />
 							<div className='container'>
 								<form>
-									<div className='row'>
+									<div className='row mb-5'>
 										<div className='col-3'>
 											<fieldset disabled>
 												<label
@@ -132,6 +181,86 @@ const DetailProject = () => {
 											<div id='emailHelp' className='form-text'></div>
 										</div>
 									</div>
+
+									<div className='d-flex flex-row'>
+										<h5>Bienes Inmuebles del Proyecto</h5>
+										<Link
+											className='justify-content-end'
+											style={{
+												display: 'flex',
+												width: 70,
+												justifyContent: 'space-between',
+												alignItems: 'center',
+												textDecoration: 'none',
+											}}
+											to='/adquisitions/real-estates/create'
+										>
+											<i
+												className='fa fa-plus-circle'
+												aria-hidden='true'
+												style={{ fontSize: 20 }}
+											></i>
+											<p>Crear</p>
+										</Link>
+									</div>
+									<Table responsive>
+										<thead>
+											<tr>
+												<th scope='col' className='align-top center'>
+													ID
+												</th>
+												<th scope='col' className='align-top'>
+													Matricula
+												</th>
+												<th scope='col' className='align-top'>
+													Nombre
+												</th>
+												<th scope='col' className='align-top'>
+													Fecha Creación
+												</th>
+												<th scope='col' className='align-top'>
+													Creado por
+												</th>
+												<th scope='col' className='align-top'>
+													<div
+														style={{
+															display: 'flex',
+															flexDirection: 'column',
+														}}
+													>
+														Acciones
+														<div
+															style={{
+																display: 'flex',
+																width: '100%',
+																justifyContent: 'space-around',
+															}}
+														>
+															<p> Ver </p>
+															<p> Editar </p>
+															<p> Eliminar </p>
+														</div>
+													</div>
+												</th>
+											</tr>
+										</thead>
+										<tbody>
+											{realEstates.map((project) => {
+												let creationDate = new Date(
+													parseFloat(project.audit_trail.created_on)
+												).toDateString();
+												return (
+													<ItemRealEstate
+														id={project.id}
+														matricula={project.registry_number}
+														name={project.name}
+														creationDate={creationDate}
+														createdBy={project.audit_trail.created_by}
+													/>
+												);
+											})}
+										</tbody>
+									</Table>
 								</form>
 							</div>
 						</div>
